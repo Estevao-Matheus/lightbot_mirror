6cd25556f1b6ae4c52e5c61f79e85a8a50d18746
Matheus Estevao Faria <estevaox@DESKTOP-ISEDIV3.localdomain>
2026-01-27 03:18:53 -0300

    feat: painel de comandos e lista de comandos clicados implementados

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ const TILE_HEIGHT = 50;
 const GRID_SIZE = 10;
 const MAX_TILE_HEIGHT = 80;
 
+let comandQuery = [];
+
 let grid = [
   [14, 23, 23, 23, 23, 23, 23, 23, 23, 13],
   [21, 32, 33, 33, 28, 33, 28, 33, 31, 20],
@@ -50,4 +52,55 @@ function draw_grid() {
 function draw() {
   background("black");
   draw_grid();
+}
+
+const buttons = document.querySelectorAll('.image-button');
+const mainMethod = document.getElementById('main-method')
+
+buttons.forEach(button => {
+    button.addEventListener('click', handleButtonClick);
+});
+
+function handleButtonClick(event) {
+  console.log(event.target);
+  const buttonValue = event.currentTarget.value;
+  console.log("Valor do botão:",buttonValue);
+   comandQuery.push(buttonValue);
+   console.log('Current Array:', comandQuery);
+   updateMainMethod();
+}
+
+function updateMainMethod() {
+    mainMethod.innerHTML = '';
+    comandQuery.forEach((comand, index) => {
+
+        const comandoElement = document.createElement('div');
+        comandoElement.className = 'image-button';
+        const imgElement = document.createElement('img');
+        
+        switch(comand) {
+            case 'frente':
+                imgElement.src = './assets/comandos/arrow_up.png';
+                imgElement.alt = 'avançar';
+                break;
+            case 'direita':
+                imgElement.src = './assets/comandos/arrow_right.png';
+                imgElement.alt = 'virar direita';
+                break;
+            case 'esquerda':
+                imgElement.src = './assets/comandos/arrow_left.png';
+                imgElement.alt = 'virar esquerda';
+                break;
+            case 'pulo':
+                imgElement.src = './assets/comandos/jump.png';
+                imgElement.alt = 'pular';
+                break;
+            case 'acender':
+                imgElement.src = './assets/comandos/lightbulb.png';
+                imgElement.alt = 'acender luz';
+                break;
+        }
+        comandoElement.appendChild(imgElement);
+        mainMethod.appendChild(comandoElement);
+    });
 }
